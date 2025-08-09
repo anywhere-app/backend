408a66fd36eb28e8e211dd8ef9a9b9a63810ec0b
Vivien Márton <vivien.marton@spsehalova.sk>
2025-08-09 14:53:48 +0200

    Implementation of user authentication

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from sqlalchemy import create_engine, Column, Integer, Boolean, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 import datetime
 from database import Base
 
@@ -9,21 +10,21 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    unique_username = Column(String, unique=True)
-    username = Column(String, default=unique_username)
-    email = Column(String)
+    username = Column(String)
+    email = Column(String, unique=True)
     hashed_password = Column(String)
-    bio = Column(String)
-    pfp_url = Column(String)
+    bio = Column(String, nullable=True)
+    pfp_url = Column(String, nullable=True)
     follower_count = Column(Integer, default=0)
     following_count = Column(Integer, default=0)
     posts_count = Column(Integer, default=0)
     likes_count = Column(Integer, default=0)
     visited_count = Column(Integer, default=0)
-    created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
-    updated_at = Column(DateTime, default=created_at)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now())
     is_suspended = Column(Boolean, default=False)
     suspended_at = Column(DateTime, nullable=True)
     suspended_until = Column(DateTime, nullable=True)
     is_admin = Column(Boolean, default=False)
-
+    is_active = Column(Boolean, default=True)
+    last_seen_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
